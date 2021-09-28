01a9b5889562f29f73b83b7a63b1a721acaf7c4a
Jonelle Solomon <nelljosolomon1@gmail.com>
2021-09-28 20:23:31 +0800

    activity 1&2

--- resources/views/contact.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('layouts.master')
-
-@section('page-title', 'Contact Page')
--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-
-@section('page-title', 'Home Page')
-
-
-@section('content')
-    <h1 class="text-center">Welcome to my store</h1>
-
-@endsection
